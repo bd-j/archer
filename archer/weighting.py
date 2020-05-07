c7cd550c2cd91c47776381e4fdcb784a0078370d
Benjamin Johnson <benjamin.duncan.johnson@gmail.com>
2020-05-07 14:36:04 -0400

    more weighting logic (MGIANTS.)

--- archer/weighting.py
+++ archer/weighting.py
@@ -4,8 +4,6 @@
 import numpy as np
 import matplotlib.pyplot as pl
 
-from .seds import Isochrone, FILTERS
-
 
 def kroupa_imf(minit):
     alpha = -2.3 * np.ones_like(minit)
@@ -28,7 +26,7 @@ def get_lf(iso, loga=10, feh=-1, afe=0):
     return mags, params, weights
 
 
-def observable_fraction(mag, wght, d_kpc, mask=True, faint=18, bright=15):
+def lf_observable_fraction(mag, wght, d_kpc, mask=True, faint=18, bright=15):
     """Given an LF ([mag, weight] pair), compute corrections for a range of distances
     """
     mu = 5 * np.log10(d_kpc) + 10
@@ -40,18 +38,47 @@ def observable_fraction(mag, wght, d_kpc, mask=True, faint=18, bright=15):
     return frac
 
 
-def correction(dist, feh, iso, masker=None,
-               bright=15, faint=18,
+def mag_weight(dist, feh, iso, masker=None,
+               bright=15, faint=18, selection_band="PS_r",
                lf_params=dict(afe=0, loga=10)):
-    """Compute correction for a single (distance, feh) pair
+    """Compute probability that a star of a given feh and distance made it
+    into the magnitude selection, based on the LF for that metallicity.
+    Takes about 50ms per object
+
+    dist : float
+        Distance in kpc
+
+    feh : float
+        [Fe/H] of the target
+
+    iso : seds.Isochrone instance
+        Assumes that the mag selection band (i.e. PS_r) is the first element
+        of the output magnitudes
+
+    masker : callable, optional
+        A function that takes a params structured array and a set of apparent
+        magnitudes and returns a boolean array indicating whether each element
+        (isochrone point) is a valid member
+
+    bright : float (optional, default 15)
+        Bright limit to have made it into the selection.
+
+    faint : float (optional, default 18)
+        Faint limit to have made it into the selection
+
+    lf_params : dict
+        Dictionary of parameters (e.g. loga, afe) for generating the isochrone & LF
     """
+    band = iso.filters == selection_band
+    assert band.sum() == 1
+
     mu = 5 * np.log10(dist) + 10
     mags, params, _ = iso.get_seds(feh=feh, apply_corr=False, dist=10,
                                    **lf_params)
     weights = imf_weight(params["mini"])
-    mag = mags[:, 0] + mu
+    mag = mags[:, band] + mu
     if masker is not None:
-        mask = masker(params, mags)
+        mask = masker(params, mags, iso.filters.tolist())
     else:
         mask = True
     valid = (mag > bright) & (mag < faint) & mask
@@ -59,42 +86,145 @@ def correction(dist, feh, iso, masker=None,
     return frac
 
 
-def correction_table(iso, distances, metallicities, masker=None,
-                     lf_params=dict(afe=0, loga=10),
-                     obs_params=dict(bright=15, faint=18)):
-    frac = np.zeros([len(metallicities), len(distances)])
-    for i, feh in enumerate(metallicities):
-        mags, params, weights = get_lf(iso, feh=feh, **lf_params)
-        if masker is not None:
-            mask = masker(params)
-        else:
-            mask = True
-        frac[i, :] = observable_fraction(mags[:, 0], weights, distances,
-                                         mask=mask, **obs_params)
-    return frac
-
-
-def gmasker(params, mags):
-    """Default apply mask based on logg
+def logg_masker(params, mags, filters):
+    """Default: apply mask based on logg
     """
     return params["logg"] < 3.5
 
 
+def mgiant_masker(params, mags, filters):
+    
+    g_r = mags[:, filters.index("PS_g")] - mags[:, filters.index("PS_r")]
+    w1_w2 = mags[:, filters.index("WISE_W1")] - mags[:, filters.index("WISE_W2")]
+    z_w1 = mags[:, filters.index("PS_z")] - mags[:, filters.index("WISE_W1")]
+    sel = ((w1_w2 > -0.4) & (w1_w2 < 0.) &
+           (g_r < 1.1) &
+           (z_w1 > 1.9) & (z_w1 < 2.5) &
+           (z_w1 > (1.2*g_r + 0.95)) & (z_w1 < (1.2*g_r + 1.15))
+           )
+    return sel
+
+
+def bhb_masker(params, mags, filters):
+    return None
+
+
+def rank_weight(rank, ptgID, wcat=None, selector=None, rcat=None):
+    """Compute the fraction of stars that made it from mag selected sample
+    (scat) into parent sample of interest given rank and ptgID
+
+    wcat : FITS_rec or structured array
+        pcat with weights
+
+    selector : optional, callable
+        A function that applies the final 'parent' sample selection
+        (e.g. logg < 3.5, snr > 3).  If not supplied, final weights will come
+        from the wcat
+    
+    rcat : optional FITS_rec or structured array
+        the full rcat.  if not supplied, final weights will come from the wcat
+    """
+    ind = wcat["PTGID"] == ptgID
+    # probability to go from scat to rcat for this rank
+    w1 = wcat[ind]["RANK{}_WGT_ALL".format(rank)]
+    
+    # probability to go from rcat to selection for this rank
+    #if (rcat is None) or (selector is None):
+    #    w1 = wcat[ind]["RANK{}_WGT_ALL".format(rank)]
+    #    w2 = 1.0
+    #else:
+    #    # compute from rcat
+    #    tileID, selID, dateID = ptgID.split('_')
+    #    inds = ((rcat["tileID"] == tileID) & (rcat["selID"] == selID) &
+    #            (rcat["dateID"] == dateID) & (rcat["XFIT_RANK"] == rank))
+    #    # number in rcat with this rank
+    #    n_ptg = inds.sum()
+    #    # number selected from rcat with this rank
+    #   n_sel = selector(rcat[inds]).sum()
+    #   w2 = n_sel*1. / n_ptg
+    return w1
+
+
+def compute_total_weight(rcat_row, iso, wcat, snr_limit=3.0):
+    row = rcat_row
+    ptgID = "{}_{}_{}".format(row["tileID"], row["selID"], row["dateID"])
+    ptg_ind = wcat["PTGID"] == ptgID
+    rank = row["XFIT_RANK"]
+    rw = wcat[ptg_ind]["RANK{}_WGT_ALL".format(rank)]
+    
+    # find PS_r s.t. SNR=3
+    coeffs = wcat[ptg_ind]["snr_curve"][0].copy()
+    coeffs[-1] -= snr_limit
+    faint_snr = min(np.roots(coeffs))
+
+    masker = logg_masker
+    
+    # get bright and faint limits for this rank
+    band = "PS_r"
+    if rank == 3:
+        faint = min(18.5, faint_snr)
+        bright = 18
+    elif (rank == 1):
+        faint = min(17.5, faint_snr)
+        bright = 13.5
+    else:
+        faint = min(18.0, faint_snr)
+        bright = 15.0
+    
+    # get isochrone masker for this selection
+    if (rank == 1) & (row["MGIANT"] > 0):
+        masker = mgiant_masker
+        #band = "WISE_W1"
+    elif (rank == 1) & (row["BHB"] > 0):
+        masker = bhb_masker
+
+    mw = mag_weight(row["dist_adpt"], row["FeH"], iso,
+                    bright=bright, faint=faint, selection_band=band,
+                    masker=masker)
+
+    return rw, mw
+
+
 if __name__ == "__main__":
+
+    from archer.seds import Isochrone, FILTERS
     from archer.config import parser, rectify_config
     config = rectify_config(parser.parse_args())
-    
-    filters = np.array(["PS_r"])
+
+    # test the mag weight code
+    feh = -2.0
+    filters = np.array(["PS_r", "PS_g", "PS_z", "WISE_W1", "WISE_W2"])
     iso = Isochrone(mistfile=config.mistiso, nnfile=config.nnfile, filters=filters)
-    mags, params, weights = get_lf(iso, feh=-1)
-    gmask = (params["logg"] < 3.5)
+    mags, params, weights = get_lf(iso, feh=feh)
+    gmask = logg_masker(params, mags, iso.filters.tolist())
+    mmask = mgiant_masker(params, mags, iso.filters.tolist())
 
     d_arr = np.linspace(2, 100, 1000)
-    frac_g = observable_fraction(mags[:, 0], weights, d_arr, mask=gmask)
-    frac = observable_fraction(mags[:, 0], weights, d_arr)
+    frac_g = lf_observable_fraction(mags[:, 0], weights, d_arr, mask=gmask)
+    frac_m = lf_observable_fraction(mags[:, 0], weights, d_arr, mask=mmask,
+                                    faint=17.5, bright=13.5)
+    frac = lf_observable_fraction(mags[:, 0], weights, d_arr)
+
 
     fig, ax = pl.subplots()
-    ax.plot(d_arr, np.log10(frac) - np.log10(frac[0]))
-    ax.plot(d_arr, np.log10(frac_g) - np.log10(frac[0]))
+    ax.plot(d_arr, np.log10(frac) - np.log10(frac[0]), label="All stars")
+    ax.plot(d_arr, np.log10(frac_g) - np.log10(frac[0]), label="logg < 3.5")
+    ax.plot(d_arr, np.log10(frac_m) - np.log10(frac[0]), label="M Giants")
     ax.set_xscale("log")
+    ax.set_xlabel("Distance (kpc)")
+    ax.set_ylabel(r"log (N$_{\rm targetable}$ / N)")
+    ax.legend()
+    ax.set_xlim(2, 50)
+    ax.set_ylim(-5, 0.2)
+    ax.set_title("[Fe/H] = {:3.1f}".format(feh))
     pl.show()
+
+    import sys
+    sys.exit()
+
+    from astropy.io import fits
+    rcat = fits.getdata(config.rcat_file)
+    pcat = None
+    for row in rcat[good & sgr]:
+        if row["BHB"] == 0:
+            rw, mw = compute_total_weight(row, iso, pcat, snr_limit=3)
--- paper/make_selection.py
+++ paper/make_selection.py
@@ -5,10 +5,7 @@ import numpy as np
 from archer.flags import flag_names, make_bitmask
 
 
-def rcat_select(rcat, rcat_r, dly=0.0, achilles_file=None,
-                allow_flags=[]):
-
-    sgr = rcat_r["ly"] < (-0.3 * rcat_r["lz"] - 2.5 + dly)
+def good_select(rcat, rcat_r, allow_flags=["hot", "high_vtan"]):
 
     # allow some bits to be set
     assert np.all([f in flag_names for f in allow_flags])
@@ -16,25 +13,27 @@ def rcat_select(rcat, rcat_r, dly=0.0, achilles_file=None,
     ok_flags = np.bitwise_xor(np.bitwise_or(rcat["FLAG_BITMASK"], bitmask), bitmask) == 0
     
     # main quality selection
-    good = (ok_flags & (rcat["SNR"] >= 3) &
+    good = (ok_flags & (rcat["SNR"] >= 3) & (rcat["V_tan"] < 900) &
             (rcat["logg"] < 3.5) & (rcat["FeH"] >= -3))
 
     # remove large L uncertainties
-    # remove large Lx and weird chemistry
+    # remove weird chemistry
     try:
         good_l = (rcat["Lz_err"] < 3e3) & (rcat["Ly_err"] < 3e3)
-        good_l = good_l & (np.abs(rcat_r["lx"]) < 10) & (rcat["afe"] < (-0.3*rcat["FeH"]+0.2))
-        good = good & good_l
+        good_c = (rcat["afe"] < (-0.3*rcat["FeH"]+0.2))
+        good = good & good_l & good_c
     except:
-        print("No L error cut")
-
-    if achilles_file:
-        raise(NotImplementedError)
-        from astropy.io import fits
-        dat = fits.getdata(achilles_file)
-        ach = np.where(rcat["starname"] == dat["starname"])
+        print("No L error or chem cut")
 
     #return np.ones(len(rcat), dtype=bool), np.ones(len(rcat), dtype=bool) 
+    return good
+
+
+def rcat_select(rcat, rcat_r, dly=0.0, allow_flags=["hot", "high_vtan"]):
+
+    sgr = rcat_r["ly"] < (-0.3 * rcat_r["lz"] - 2.5 + dly) 
+    sgr = sgr & (np.abs(rcat_r["lx"]) < 10)
+    good = good_select(rcat, rcat_r, allow_flags=allow_flags)
     return good, sgr
 
 
